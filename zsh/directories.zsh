e525968ceab7f65cbace00800edbda88b7072a12
Zack Amiton <zamiton@outlook.com>
2023-11-18 16:45:54 -0500

    revamped lyrics

--- zsh/directories.zsh
+++ zsh/directories.zsh
@@ -3,3 +3,7 @@ alias s="/Users/zackamiton/Code/BrownCS"
 alias site="/Users/zackamiton/Code/Zamiton/webapp"
 alias cla="/Users/zackamiton/Library/LaunchAgents/"
 alias cld="/Library/LaunchDaemons/"
+
+# temporary aliases
+alias networks="/Users/zackamiton/Code/BrownCS/Networks/container/; ./run-container"
+alias networks-code="/Users/zackamiton/Code/BrownCS/Networks/container/home/iptcp-zoren/"
--- zsh/tuningfork.zsh
+++ zsh/tuningfork.zsh
@@ -20,7 +20,18 @@ function qc()  { $_TUNINGFORK -st $@ --share }
 function qca() { $_TUNINGFORK -st $@ --share APPLE }
 function qrl() { $_TUNINGFORK -x LIBRARY -# -1 $@ }    
 function qrb() { $_TUNINGFORK -x BACKLOG -# -1 $@ }
+function qtm() { $_TUNINGFORK $@ --share --text mahlon }
 
 # Lyrics Aliases
-function lyrics() { $_LYRICS $@ | less; }
+function lyrics() { 
+    paths=$($_LYRICS $@);
+    code=$?   
+    if [[ $code == 0 ]] then
+        echo $paths | xargs less;
+        echo $paths | xargs rm;
+    else
+        echo $paths;
+    fi
+}
+
 function ql() { $_LYRICS }
