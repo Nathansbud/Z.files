353b732e4bb4e34ad07016923037d5a871c83157
Zack Amiton <zack@duolingo.com>
2024-06-19 14:09:39 -0400

    add comments + scratch space

--- zsh/directories.zsh
+++ zsh/directories.zsh
@@ -1,5 +1,8 @@
 alias c="$PERSONAL_DIR"
 alias s="$WORK_DIR"
+
 alias site="$PERSONAL_DIR/Zamiton/webapp"
+alias configs="$PERSONAL_DIR/Z.files"
+
 alias cla="$HOME_DIR/Library/LaunchAgents/"
 alias cld="/Library/LaunchDaemons/"
---
+++ zsh/temporary.zsh
@@ -0,0 +1,3 @@
+# Short-term aliases, such as quick directory access while working on 
+# a class project, debug testing, etc; designed to be a dumping ground
+# for any and all quick configs
